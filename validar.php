8bdd6b7eba4b53f7b058ba6171fe1668be03167a
xeverland <83352027+xeverland@users.noreply.github.com>
2022-10-29 20:23:02 -0300

    nuevos_cambios

--- validar.php
+++ validar.php
@@ -2,17 +2,18 @@
 $name = $_POST['nombre'];
 $message = $_POST['mensaje'];
 
-$header = 'From: ' + " \r\n";
+$header = 'From: ' $nombre " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
 $message = "Este mensaje fue enviado por: " . $name . " \r\n";
-$message .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$message .= "mensaje: " . $_POST['mensaje'] . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
 $para = 'bcaullan777@gmail.com';
-$asunto = 'Mensaje de... ( como quierEscribees que se vea el remitente de tu correo)';
+$asunto = 'ASUNTO';
+
 
 mail($para, $asunto, utf8_decode($message), $header);
 
